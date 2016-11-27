7fd8f74441f7739c90658db40f2e581fcb414503
Hopper262 <hopper@whpress.com>
2016-11-27 15:12:15 -0500

    Clarify content licensing (resolves Aleph-One-Marathon/alephone#30)

--- Plugins/Default HUD/Default_HUD_M1.lua
+++ Plugins/Default HUD/Default_HUD_M1.lua
@@ -1,4 +1,19 @@
--- Default HUD for Marathon, version 1.1
+-- Copyright (C) 2011 and beyond by Jeremiah Morris
+-- and the "Aleph One" developers.
+-- 
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- This license is contained in the file "COPYING",
+-- which is included with this source code; it is available online at
+-- http://www.gnu.org/licenses/gpl.html
 
 Triggers = {}
 function Triggers.draw()
